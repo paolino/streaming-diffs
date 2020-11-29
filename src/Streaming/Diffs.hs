4d38a7acdfc049594e027e0d3dc627f0f63dcf7c
Paolo Veronelli <Paolo.Veronelli@gmail.com>
2020-11-29 12:55:37 +0100

    add groupBatch

--- src/Streaming/Diffs.hs
+++ src/Streaming/Diffs.hs
@@ -1,26 +1,28 @@
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 module Streaming.Diffs where
 
-import qualified Data.Map.Monoidal.Strict as M
-import qualified Data.Map.Strict as Mn
+import Control.Arrow (Arrow ((***)))
+import qualified Data.Map.Monoidal.Strict as Mn
+import qualified Data.Map.Strict as M
 import Protolude
 import Streaming
+import qualified Streaming as S
 import Streaming.Internal
 import qualified Streaming.Prelude as S
 
-breaker ::
-  (Eq t, Monad m) =>
-  (a -> t) ->
-  Stream (Of a) m r ->
-  Stream (Of a) m (Either r (t, Stream (Of a) m r))
+breaker
+  :: (Eq t, Monad m)
+  => (a -> t)
+  -> Stream (Of a) m r
+  -> Stream (Of a) m (Either r (t, Stream (Of a) m r))
 breaker _ (Return r) = Return (Left r)
 breaker tag s = Effect do
   x <- inspect s
@@ -39,10 +41,10 @@ breaker tag s = Effect do
                   then Step $ y :> go t stream'
                   else Return $ Right (t', Step $ y :> stream')
 
-integrate ::
-  (Eq t, Monad m, Monoid b) =>
-  Stream (Of (t, b)) m r ->
-  Stream (Of (t, b)) m r
+integrate
+  :: (Eq t, Monad m, Monoid b)
+  => Stream (Of (t, b)) m r
+  -> Stream (Of (t, b)) m r
 integrate stream = Effect $ do
   b :> mr <- S.foldMap snd $ breaker fst stream
   pure $ case mr of
@@ -74,11 +76,11 @@ lrb2Update (Last (Just x), Last (Just y))
 lrb2Update (Last (Just x), _) = Just $ Delete x
 lrb2Update (_, Last (Just y)) = Just $ New y
 
-streamDiffs ::
-  (Monad m, Functor f, Ord t, Monoid (f (LRB a)), Eq a) =>
-  Stream (Of (t, f a)) m r ->
-  Stream (Of (t, f a)) m r ->
-  Stream (Of (t, f (Maybe (Update a)))) m (r, r)
+streamDiffs
+  :: (Monad m, Functor f, Ord t, Monoid (f (LRB a)), Eq a)
+  => Stream (Of (t, f a)) m r
+  -> Stream (Of (t, f a)) m r
+  -> Stream (Of (t, f (Maybe (Update a)))) m (r, r)
 streamDiffs old new =
   S.map (fmap $ fmap lrb2Update) $
     mergeSemigroup (prepare mkL old) (prepare mkR new)
@@ -87,28 +89,67 @@ streamDiffs old new =
 
 data Update v = Update v v | Delete v | New v deriving (Show, Eq)
 
-streamAssocsDiffs ::
-  (Monad m, Ord t, Ord k, Eq v) =>
-  Stream (Of (t, (k, v))) m r ->
-  Stream (Of (t, (k, v))) m r ->
-  Stream (Of (t, (k, Update v))) m (r, r)
+streamAssocsDiffs
+  :: (Monad m, Ord t, Ord k, Eq v)
+  => Stream (Of (t, (k, v))) m r
+  -> Stream (Of (t, (k, v))) m r
+  -> Stream (Of (t, (k, Update v))) m (r, r)
 streamAssocsDiffs s1 s2 =
   S.for
-    do { streamDiffs (S.map prepare s1) (S.map prepare s2) }
+    do streamDiffs (S.map prepare s1) (S.map prepare s2)
     do \(t, q) -> S.catMaybes $ S.each $ (\(k, v) -> (t,) . (k,) <$> v) <$> M.assocs q
   where
     prepare (t, (k, v)) = (t, M.singleton k v)
 
-preemptive :: Stream (Of Text) IO b  -> Stream (Of Text) IO b
+preemptive :: MonadIO m => Stream (Of a) IO b -> Stream (Of a) m b
 preemptive s = do
-  c <- lift $ do
+  c <- liftIO $ do
     c <- newEmptyMVar
-    forkIO $ void $ S.effects $ S.mapM 
-      do  \x -> threadDelay 1_000_000 >> putText ("input " <> x) >> putMVar c x 
-      do s
+    forkIO $
+      void $
+        S.effects $ S.chain
+          do putMVar c
+          do s
     pure c
   forever $ do
     liftIO (readMVar c) >>= S.yield >> liftIO (takeMVar c) >> pure ()
 
 testPreemptive :: Stream (Of ()) IO ()
-testPreemptive = S.mapM (\l -> threadDelay 2_000_000 >> putText ("output " <> l)) $ preemptive $ S.map show $ S.each [1..]
+testPreemptive =
+  S.mapM
+    do \o -> threadDelay 2_000_000 >> putText ("output " <> o)
+    do
+      preemptive $ S.chain
+        do \i -> threadDelay 1_000_000 >> putText ("input " <> i)
+        do S.map show $ S.each [1 ..]
+
+preemptiveOut :: (a -> IO ()) -> Stream (Of a) IO b -> Stream (Of a) IO b
+preemptiveOut out s = do
+  c <- liftIO $ do
+    c <- newEmptyMVar
+    forkIO $ forever do
+      x <- readMVar c
+      out x
+      void $ takeMVar c
+    pure c
+  S.chain
+    do putMVar c
+    do s
+
+groupBatch :: (Ord k, Monad m) 
+  => Int -- ^ batch size 
+  -> (a -> (k, c))  -- ^ bucket selection
+  -> Stream (Of a) m b  
+  -> Stream (Of (k, [c])) m b
+groupBatch dumpSize bucket = go mempty
+  where
+    go m s = effect do
+      x <- S.inspect s
+      pure $ case x of
+        Left b -> S.each (M.assocs $ reverse . snd <$> m) >> pure b
+        Right (x :> s') ->
+          let (k, y) = bucket x
+              (l, ys) = (succ *** (y :)) $ fromMaybe (0, []) $ M.lookup k m
+           in if l >= dumpSize
+                then S.yield (k, reverse ys) >> go (M.delete k m) s'
+                else go (M.insert k (l, ys) m) s'
